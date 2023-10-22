665d6a969ae200bcc8dd5b6615a0bac8b7faae4b
Leonardo Diana <leonardodiana@leonardos-air.homenet.telecomitalia.it>
2023-10-22 17:10:17 +0200

    male

--- crud.py
+++ crud.py
@@ -3,91 +3,125 @@ from pydantic import BaseModel
 import sqlite3
 from models import *
 
-app= FastAPI()
+app = FastAPI()
 
 import sqlite3
 
+
 def create_connection():
- connection = sqlite3.connect("db.sqlite3")
- return connection
+    connection = sqlite3.connect("db.sqlite3")
+    return connection
+
+
+# CREATE
+
 
 def create_structure(structure: StructureCreate):
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("INSERT INTO structure(id, name) VALUES (?,?)", (structure.id, structure.name))
- connection.commit()
- connection.close()
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+        "INSERT INTO structure(id, name) VALUES (?,?)", (structure.id, structure.name)
+    )
+    connection.commit()
+    connection.close()
+
 
 def create_info(info: InfoCreate):
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("INSERT INTO info(id, region, year, presenze, arrivi, structure) VALUES (?,?,?,?,?,?)", (info.id, info.region, info.year, info.presenze, info.arrivi, info.structure))
- connection.commit()
- connection.close()
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+        "INSERT INTO info(id, region, year, presenze, arrivi, structure) VALUES (?,?,?,?,?,?)",
+        (info.id, info.region, info.year, info.presenze, info.arrivi, info.structure),
+    )
+    connection.commit()
+    connection.close()
 
 
-def read_structure(id:int):
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("SELECT * FROM structure where id = ?", (id,))
- result = cursor.fetchone()
- connection.commit()
- connection.close()
- return result
+# READ
 
-def read_structure_by_name(name:str):
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("SELECT * FROM structure where name = ?", (name,))
- result = cursor.fetchone()
- connection.commit()
- connection.close()
- return result
+
+def read_structure(id: int):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM structure where id = ?", (id,))
+    result = cursor.fetchone()
+    connection.commit()
+    connection.close()
+    return result
 
 
 def read_all_structures():
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("SELECT * FROM structure")
- result = cursor.fetchall()
- connection.commit()
- connection.close()
- return result
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM structure")
+    result = cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return result
+
 
 def read_all_infos():
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("SELECT * FROM info")
- result = cursor.fetchall()
- connection.commit()
- connection.close()
- return result
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM info")
+    result = cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return result
 
-def read_info_by_year(year:int):
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("SELECT * FROM info where year= ?",(year,))
- result = cursor.fetchall()
- connection.commit()
- connection.close()
- return result
 
-def read_info_by_region(region:str):
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("SELECT * FROM info where region= ?",(region,))
- result = cursor.fetchall()
- connection.commit()
- connection.close()
- return result
+def read_info_by_year(year: int):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM info where year= ?", (year,))
+    result = cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return result
 
-def read_info_by_structure_and_region(id_structure:int, region:str):
- connection = create_connection()
- cursor = connection.cursor()
- cursor.execute("SELECT * FROM structure as s INNER JOIN info as i on s.id=i.structure WHERE s.id=? and i.region = ?",(id_structure, region,))
- result = cursor.fetchall()
- connection.commit()
- connection.close()
- return result
 
-print(read_info_by_structure_and_region(0,'Piemonte'))
+def read_info_by_region(region: str):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM info where region= ?", (region,))
+    result = cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return result
+
+
+def read_info_by_structure_and_region(id_structure: int, region: str):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute(
+        "SELECT * FROM structure as s INNER JOIN info as i on s.id=i.structure WHERE s.id=? and i.region = ?",
+        (
+            id_structure,
+            region,
+        ),
+    )
+    result = cursor.fetchall()
+    connection.commit()
+    connection.close()
+    return result
+
+# def search_info(year:int=None, region:str=None):
+#     #Build query
+#     query="SELECT * FROM info"
+#     conditions=[]
+#     if year is not None:
+#         conditions.append("year = ?")
+#     if region is not None:
+#         conditions.append("region = ?")
+#     if conditions:
+#         query += " WHERE " + " AND ".join(conditions)
+#     connection = create_connection()
+#     cursor = connection.cursor()
+#     cursor.execute(query,(year,))
+#     result = cursor.fetchall()
+#     connection.commit()
+#     connection.close()
+#     return result
+
+
+#print(read_info_by_year(2011))
--- main.py
+++ main.py
@@ -5,43 +5,52 @@ from crud import *
 from models import *
 
 
-app= FastAPI()
+app = FastAPI()
+
 
 @app.post("/structure")
-def create_structure_endpoint(structure : StructureCreate):
+def create_structure_endpoint(structure: StructureCreate):
     structure_id = create_structure(structure)
     return {"id": structure_id, **structure.model_dump()}
 
+
 @app.post("/info")
-def create_info_endpoint(info : InfoCreate):
+def create_info_endpoint(info: InfoCreate):
     info_id = create_info(info)
     return {"id": info_id, **info.model_dump()}
 
-@app.get("/structure/{id}")
-def read_structure_endpoint(id:int):
-  return read_structure(id)
 
-@app.get("/structure/{name}")
-def read_structure_by_name_endpoint(name:str, q: str | None = None):
-  return read_structure_by_name(name)
+@app.get("/structure/{id}")
+def read_structure_endpoint(id: int):
+    return read_structure(id)
+
 
 @app.get("/structure")
 def read_all_structure_endpoint():
-  return read_all_structures()
+    return read_all_structures()
 
-@app.get("/info")
-def read_all_infos_endpoint():
-   return read_all_infos()
 
-@app.get("/info/{region}")
-def info_by_region_endpoint(region: str, q: str | None = None):
-  return read_info_by_region(region)
+# @app.get("/info")
+# def read_all_infos_endpoint(year: int | None = None, region: str | None = None):
+#     if(year!=None):
+#       return read_info_by_year(year)
+#     if(region!=None):
+#         return read_info_by_region(region)
+#     if(region==None and year==None):
+#         return read_all_infos()
 
 @app.get("/info/{year}")
-def info_by_year_endpoint(year:int):
-  return read_info_by_year(year)
+def info_by_year_endpoint(year: int):
+    return read_info_by_year(year)
+
+@app.get("/info-region/{region}")
+def info_by_region_endpoint(region: str, q: str | None = None):
+    return read_info_by_region(region)
 
 @app.get("/join")
-def info_join_structure_region_endpoint(id_structure:int, region:str, q: str | None = None):
-  return read_info_by_structure_and_region(id_structure, region)
+def info_join_structure_region_endpoint(id_structure: int, region: str, q: str | None = None):
+    return read_info_by_structure_and_region(id_structure, region)
 
+# @app.get("/search")
+# def info_search_endpoint(year:int=None, region:str=None):
+#     search_info(year, region)
